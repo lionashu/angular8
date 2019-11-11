0587750b16da4b68561038071147fcafa51bfc10
lionashu <ashishmipra@gmil.com>
2019-11-11 19:52:22 +0530

    update nav Bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { AboutComponent } from './about/about.component'
+import { from } from 'rxjs';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', component: HomeComponent},
+{path:'list', component: ListComponent},
+{path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
